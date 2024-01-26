c6b8c5ffe9a6520b4ed8e321c3474598bcbe7a40
Austin Richards <austinjrichards@outlook.com>
2024-01-26 06:54:21 -0700

    Dynamically render home page

--- stoic_api/web/views.py
+++ stoic_api/web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home_view(request):
-    return HttpResponse('Hello world', content_type='text/plain')
+    return render(request, 'web/index.html', {})
 
